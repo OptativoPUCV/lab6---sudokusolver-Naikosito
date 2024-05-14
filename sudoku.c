3ec99f0a52888ff95db3145cb1d1e2e0bd92cf74
Your Name <you@example.com>
2024-05-14 00:38:38 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -100,7 +100,14 @@ List* get_adj_nodes(Node* n){
 
 
 int is_final(Node* n){
-    return 0;
+    for(int i = 0; i < 9; i++){
+        for(int j = 0; j < 9; j++){
+            if(n->sudo[i][j] == 0){
+                return 0; // El nodo no es un estado final
+            }
+        }
+    }
+    return 1; // El nodo es un estado final
 }
 
 Node* DFS(Node* initial, int* cont){
